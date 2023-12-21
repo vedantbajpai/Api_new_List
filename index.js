b161a9e647ab3c071ffdd162838772e0f378a66a
Vedant Bajpai <113709069+vedantbajpai@users.noreply.github.com>
2023-12-21 12:22:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const express = require('express')
+const dotenv = require('dotenv')
+const buf = Buffer.from('BASIC=basic')
+const config = dotenv.parse(buf) // will return an object
+console.log(typeof config, config) // object { BASIC : 'basic' }
 var app = express();
 const PORT = 3000;
 import('node-fetch').then(fetchModule => {
